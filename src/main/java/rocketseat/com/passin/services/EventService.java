5498c6b6c0331936aee0733b6db01a66873337b0
Luis Felipe <luisfelipe7014feups@gmail.com>
2024-04-11 09:05:26 -0300

    first commit

---
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -0,0 +1,22 @@
+package rocketseat.com.passin.services;
+
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.repositories.EventRepository;
+
+
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+    private final EventRepository eventRepository;
+
+    public void getEventDetail(String eventId){
+        Event event =  this.eventRepository.findById(eventId).orElseThrow(()-> new RuntimeException("Event not found with ID: "+ eventId));
+        return ;
+    }
+}
