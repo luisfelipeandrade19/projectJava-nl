792aa84873b48ce9b3809dbf7165925f06263c3b
Luis Felipe <luisfelipe7014feups@gmail.com>
2024-04-11 16:17:43 -0300

    created DTO's codes

--- src/main/java/rocketseat/com/passin/services/EventService.java
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -7,10 +7,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 import rocketseat.com.passin.domain.attendee.Attendee;
 import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
 import rocketseat.com.passin.dto.event.EventResponseDTO;
 import rocketseat.com.passin.repositories.AttendeeRepository;
 import rocketseat.com.passin.repositories.EventRepository;
 
+import java.text.Normalizer;
 import java.util.List;
 
 
@@ -26,7 +29,23 @@ public class EventService {
         return new EventResponseDTO(event, attendeeList.size());
     }
 
-    public void createEvent(){
+    public EventIdDTO createEvent(EventRequestDTO eventDTO){
+        Event newEvent = new Event();
+        newEvent.setTitle(eventDTO.title());
+        newEvent.setDetails(eventDTO.details());
+        newEvent.setMaximumAttendees(eventDTO.maximumAttendees());
+        newEvent.setSlug(this.createSlug(eventDTO.title()));
 
+        this.eventRepository.save(newEvent);
+
+        return  new EventIdDTO(newEvent.getId());
+    }
+
+    private String createSlug(String text){
+        String normalize = Normalizer.normalize(text, Normalizer.Form.NFD);
+        return normalize.replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
+        .replaceAll("[^\\w\\s]", "")
+                .replaceAll("\\s+", "-")
+                .toLowerCase();
     }
 }
