567c1870ea53e2c0d8a76d282184617d9410a9a4
Luis Felipe <luisfelipe7014feups@gmail.com>
2024-05-03 15:26:34 -0300

    tratando erros

---
+++ src/main/java/rocketseat/com/passin/config/ExceptionEntityHandler.java
@@ -0,0 +1,4 @@
+package rocketseat.com.passin.config;
+
+public class ExceptionEntityHandler {
+}
--- src/main/java/rocketseat/com/passin/services/EventService.java
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 import rocketseat.com.passin.domain.attendee.Attendee;
 import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.domain.event.exceptions.EventNotFoundException;
 import rocketseat.com.passin.dto.event.EventIdDTO;
 import rocketseat.com.passin.dto.event.EventRequestDTO;
 import rocketseat.com.passin.dto.event.EventResponseDTO;
@@ -24,7 +25,7 @@ public class EventService {
     private final AttendeeRepository attendeeRepository;
 
     public EventResponseDTO getEventDetail(String eventId){
-        Event event =  this.eventRepository.findById(eventId).orElseThrow(()-> new RuntimeException("Event not found with ID: "+ eventId));
+        Event event =  this.eventRepository.findById(eventId).orElseThrow(()-> new EventNotFoundException("Event not found with ID: "+ eventId));
         List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
         return new EventResponseDTO(event, attendeeList.size());
     }
